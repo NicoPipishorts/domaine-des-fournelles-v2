592bcc6e6182aa268af8ade76df7a39f679613d1
NicoPipishorts <87517338+NicoPipishorts@users.noreply.github.com>
2022-04-25 09:55:25 +0200

    Landing component created > creating Comp-Landing-Structure branch

--- src/components/App/index.js
+++ src/components/App/index.js
@@ -1,23 +1,14 @@
+//-- IMPORT NPMS
 
-import './style.scss';
+//-- IMPORT ASSETS
+import Landing from "../Landing"
+import './styles.scss';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Landing />
+    </>
   );
 }
 
---
+++ src/components/Landing/index.js
@@ -0,0 +1,14 @@
+//-- IMPORT NPMS
+
+//-- IMPORT ASSETS
+import './styles.scss';
+
+const Landing = () => {
+
+  return (
+    <div> This is the landing page </div>
+  );
+
+}
+
+export default Landing;
