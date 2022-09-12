93180d233c1ceec59052bd1e416397191566aeeb
NicoPipishorts <87517338+NicoPipishorts@users.noreply.github.com>
2022-09-12 15:07:32 +0200

    videos folder added gitignore

---
+++ doamine-des-fournelles/src/components/App/index.js
@@ -0,0 +1,25 @@
+// -- IMPORT NPM
+
+// -- IMPORT ASSETS
+import './styles.scss';
+
+// --  IMPORT COMPONENTS
+
+const App = () => {
+
+  return (
+
+    <div className="wrapper"> 
+
+      <div className="container-hero">
+
+
+      </div>
+    
+    </div>
+
+  )
+
+}
+
+export default App;
