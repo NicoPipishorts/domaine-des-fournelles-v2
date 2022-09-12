6156529951751954fe7b2545fbf2f66c0455e31b
NicoPipishorts <87517338+NicoPipishorts@users.noreply.github.com>
2022-09-12 15:08:06 +0200

    commit yarn stagged files

--- doamine-des-fournelles/src/components/App/index.js
+++ doamine-des-fournelles/src/components/App/index.js
@@ -13,6 +13,13 @@ const App = () => {
 
       <div className="container-hero">
 
+        <div className="container-hero__video">
+
+        </div>
+
+        <div className="container-hero__logo">
+          
+        </div>
 
       </div>
     
