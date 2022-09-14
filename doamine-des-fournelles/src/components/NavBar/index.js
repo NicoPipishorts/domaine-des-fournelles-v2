aeb319c3ae99d95ab36860c912ca1c72bd432cbc
NicoPipishorts <87517338+NicoPipishorts@users.noreply.github.com>
2022-09-14 11:03:38 +0200

    NavBar Icon set created and added

--- doamine-des-fournelles/src/components/NavBar/index.js
+++ doamine-des-fournelles/src/components/NavBar/index.js
@@ -7,6 +7,9 @@ import React from 'react';
 import './styles.scss';
 import IconDomaine from '../../assets/icons/domaine.png';
 import IconWine from '../../assets/icons/wine.png';
+import IconNews from '../../assets/icons/news.png';
+import IconBoutique from '../../assets/icons/boutique.png';
+import IconContact from '../../assets/icons/contact.png';
 
 const NavBar = () => {
 
@@ -17,18 +20,25 @@ const NavBar = () => {
       <nav className="navbar">
         <ul className="navbar__container">
           <li className="navbar__buttons navbar__buttons--domaine">
-            Domaine 
+            {/* Domaine */}
             <img src={IconDomaine} alt='Button Domaine' className='navbar__buttons--icons' />
           </li>  
           <li className="navbar__buttons navbar__buttons--vins">
-            Vins 
+            {/* Vins */}
             <img src={IconWine} alt='Button Domaine' className='navbar__buttons--icons' />
           </li>  
           <li className="navbar__buttons navbar__buttons--boutique">
-            Boutique
+            {/* News */}
+            <img src={IconNews} alt='Button Domaine' className='navbar__buttons--icons' />
           </li>  
-          <li className="navbar__buttons navbar__buttons--news">News</li>  
-          <li className="navbar__buttons navbar__buttons--contact">Contact</li>  
+          <li className="navbar__buttons navbar__buttons--news">
+            {/* Boutique */}
+            <img src={IconBoutique} alt='Button Domaine' className='navbar__buttons--icons' />
+          </li>  
+          <li className="navbar__buttons navbar__buttons--contact">
+            {/* Contact */}
+            <img src={IconContact} alt='Button Domaine' className='navbar__buttons--icons' />
+          </li>  
         </ul>
       </nav> 
 
