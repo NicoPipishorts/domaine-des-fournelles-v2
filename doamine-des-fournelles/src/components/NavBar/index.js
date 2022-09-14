5abb64b7aab41b2f32f368605e2abf64bbacb59f
NicoPipishorts <87517338+NicoPipishorts@users.noreply.github.com>
2022-09-14 12:11:56 +0200

    NavBar Hover done : shows label when hovering on button

--- doamine-des-fournelles/src/components/NavBar/index.js
+++ doamine-des-fournelles/src/components/NavBar/index.js
@@ -5,11 +5,11 @@ import React from 'react';
 
 // -- IMPORT ASSETS
 import './styles.scss';
-import IconDomaine from '../../assets/icons/domaine.png';
-import IconWine from '../../assets/icons/wine.png';
-import IconNews from '../../assets/icons/news.png';
-import IconBoutique from '../../assets/icons/boutique.png';
-import IconContact from '../../assets/icons/contact.png';
+import IconDomaine from '../../assets/icons/domaine-w.png';
+import IconWine from '../../assets/icons/wine-w.png';
+import IconNews from '../../assets/icons/news-w.png';
+import IconBoutique from '../../assets/icons/boutique-w.png';
+import IconContact from '../../assets/icons/contact-w.png';
 
 const NavBar = () => {
 
@@ -19,26 +19,36 @@ const NavBar = () => {
       
       <nav className="navbar">
         <ul className="navbar__container">
-          <li className="navbar__buttons navbar__buttons--domaine">
-            {/* Domaine */}
-            <img src={IconDomaine} alt='Button Domaine' className='navbar__buttons--icons' />
-          </li>  
-          <li className="navbar__buttons navbar__buttons--vins">
-            {/* Vins */}
-            <img src={IconWine} alt='Button Domaine' className='navbar__buttons--icons' />
-          </li>  
-          <li className="navbar__buttons navbar__buttons--boutique">
-            {/* News */}
-            <img src={IconNews} alt='Button Domaine' className='navbar__buttons--icons' />
-          </li>  
-          <li className="navbar__buttons navbar__buttons--news">
-            {/* Boutique */}
-            <img src={IconBoutique} alt='Button Domaine' className='navbar__buttons--icons' />
-          </li>  
-          <li className="navbar__buttons navbar__buttons--contact">
-            {/* Contact */}
-            <img src={IconContact} alt='Button Domaine' className='navbar__buttons--icons' />
-          </li>  
+          <div className='navbar__buttons--parent'>
+            <li className="navbar__buttons navbar__buttons--domaine">
+              <img src={IconDomaine} alt='Button Domaine' className='navbar__buttons--icons' />
+            </li>  
+            <span>Domaine</span>
+          </div>
+          <div className='navbar__buttons--parent'>
+            <li className="navbar__buttons navbar__buttons--vins">
+              <img src={IconWine} alt='Button Domaine' className='navbar__buttons--icons' />
+            </li>  
+            <span>Nos Vins</span>
+          </div>
+          <div className='navbar__buttons--parent'>
+            <li className="navbar__buttons navbar__buttons--news">
+              <img src={IconNews} alt='Button Domaine' className='navbar__buttons--icons' />
+            </li>  
+            <span>News</span>
+          </div>
+          <div className='navbar__buttons--parent'>
+            <li className="navbar__buttons navbar__buttons--boutique">
+              <img src={IconBoutique} alt='Button Domaine' className='navbar__buttons--icons' />
+            </li>  
+            <span>Boutique</span>
+          </div>
+          <div className='navbar__buttons--parent'>
+            <li className="navbar__buttons navbar__buttons--contact">
+              <img src={IconContact} alt='Button Domaine' className='navbar__buttons--icons' />
+            </li>
+            <span>Contact</span>
+          </div>
         </ul>
       </nav> 
 
