dc8e8111befd7e6622b016632278c9d15abd4aba
NicoPipishorts <87517338+NicoPipishorts@users.noreply.github.com>
2022-09-14 12:31:27 +0200

    Modified the label font-weight and colors

--- doamine-des-fournelles/src/components/NavBar/index.js
+++ doamine-des-fournelles/src/components/NavBar/index.js
@@ -23,31 +23,31 @@ const NavBar = () => {
             <li className="navbar__buttons navbar__buttons--domaine">
               <img src={IconDomaine} alt='Button Domaine' className='navbar__buttons--icons' />
             </li>  
-            <span>Domaine</span>
+            <span className='navbar__buttons--domaine-label'>Domaine</span>
           </div>
           <div className='navbar__buttons--parent'>
             <li className="navbar__buttons navbar__buttons--vins">
               <img src={IconWine} alt='Button Domaine' className='navbar__buttons--icons' />
             </li>  
-            <span>Nos Vins</span>
+            <span className='navbar__buttons--vins-label'>Nos Vins</span>
           </div>
           <div className='navbar__buttons--parent'>
             <li className="navbar__buttons navbar__buttons--news">
               <img src={IconNews} alt='Button Domaine' className='navbar__buttons--icons' />
             </li>  
-            <span>News</span>
+            <span className='navbar__buttons--news-label'>News</span>
           </div>
           <div className='navbar__buttons--parent'>
             <li className="navbar__buttons navbar__buttons--boutique">
               <img src={IconBoutique} alt='Button Domaine' className='navbar__buttons--icons' />
             </li>  
-            <span>Boutique</span>
+            <span className='navbar__buttons--boutique-label'>Boutique</span>
           </div>
           <div className='navbar__buttons--parent'>
             <li className="navbar__buttons navbar__buttons--contact">
               <img src={IconContact} alt='Button Domaine' className='navbar__buttons--icons' />
             </li>
-            <span>Contact</span>
+            <span className='navbar__buttons--contact-label'>Contact</span>
           </div>
         </ul>
       </nav> 
