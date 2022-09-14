f86b32578161ecd241c643ed0c2c30384f5c3f9b
NicoPipishorts <87517338+NicoPipishorts@users.noreply.github.com>
2022-09-14 14:01:02 +0200

    Page View directories created

---
+++ doamine-des-fournelles/src/components/Wines/index.js
@@ -0,0 +1,18 @@
+// -- IMPORT NPM
+
+// --  IMPORT COMPONENTS
+
+// -- IMPORT ASSETS
+import './styles.scss';
+
+const Wines = () => {
+
+  return (
+
+    <></>
+
+  )
+
+}
+
+export default Wines;
