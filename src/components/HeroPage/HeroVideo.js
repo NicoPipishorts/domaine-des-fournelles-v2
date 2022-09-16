9a726534791b2dbb8ff0cbe060b1595cd1ebb17c
NicoPipishorts <87517338+NicoPipishorts@users.noreply.github.com>
2022-09-16 13:40:47 +0200

    updated HeroPage video size

--- src/components/HeroPage/HeroVideo.js
+++ src/components/HeroPage/HeroVideo.js
@@ -5,7 +5,7 @@ import React from 'react';
 
 // -- IMPORT ASSETS
 import './styles.scss';
-import Video from '../../assets/videos/HeroVideo.mov';
+import Video from '../../assets/videos/HeroVideo-720.mp4';
 
 const HeroVideo = () => {
 
