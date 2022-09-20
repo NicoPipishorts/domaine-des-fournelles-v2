408725d2e4fab33e5ba2af63ee54f8ede04753e1
NicoPipishorts <87517338+NicoPipishorts@users.noreply.github.com>
2022-09-20 20:30:51 +0200

    fixed issue with BG on bottom of page, whitespace removed

--- src/components/WinesPage/BottleView.js
+++ src/components/WinesPage/BottleView.js
@@ -66,7 +66,7 @@ const BottleWiew = ( {
             </ul>
           </aside>
       
-          <div className={`winespage__bottles--info-hve`}>
+          <div className={`winespage__bottles--info-hve winespage__bottles--info-hve-${bottle}`}>
           La Haute Valeur <br />Environnementale
           </div>
           
