f6af8ff6495f2a79639dc4127700b1a77bd6067c
Nicolas Pisar <shamanproto@gmail.com>
2026-04-27 12:14:48 +0200

    updated boutique link

--- src/components/NavBar/Button.jsx
+++ src/components/NavBar/Button.jsx
@@ -7,7 +7,7 @@ import { useLocation, NavLink } from 'react-router-dom';
 // -- IMPORT ASSETS
 import './styles.scss';
 
-const NavBarButton = ( {icon, section, title} ) => {
+const NavBarButton = ( {icon, section, title, outsideUrl} ) => {
 
   const location = useLocation();
   const pathname = location.pathname.substring(1);
@@ -17,15 +17,27 @@ const NavBarButton = ( {icon, section, title} ) => {
     selected = true;
   }
 
-  return (   
-
-    <NavLink to={`/${section}`} style={{ textDecoration: 'none' }}>
+  const content = (
     <div className='navbar__buttons--parent'>
       <li className={ selected ? `navbar__buttons navbar__buttons--${section}-selected` : `navbar__buttons navbar__buttons--${section}`}>
         <img src={icon} alt={`Button ${title}`} className='navbar__buttons--icons' />
       </li>  
       <span className={`navbar__buttons--${section}-label`}>{title}</span>
     </div>
+  );
+
+  if( outsideUrl ){
+    return (
+      <a href={outsideUrl} target="_blank" rel="noopener noreferrer" style={{ textDecoration: 'none' }}>
+        {content}
+      </a>
+    )
+  }
+
+  return (   
+
+    <NavLink to={`/${section}`} style={{ textDecoration: 'none' }}>
+      {content}
     </NavLink>
 
   )
